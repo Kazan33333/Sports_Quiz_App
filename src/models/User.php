019c4b80ef256a0ebef6c0010bfe63f9d85ff9d8
Kazan33333 <kazan.kk@gmail.com>
2022-06-07 00:04:50 +0200

    user php

--- src/models/User.php
+++ src/models/User.php
@@ -1,1 +1,46 @@
 <?php
+
+class User
+{
+    private string $nickname;
+    private string $password;
+    private string $email;
+
+    public function getNickname(): string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): void
+    {
+        $this->nickname = $nickname;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function __construct(string $nickname, string $password, string $email)
+    {
+        $this->nickname = $nickname;
+        $this->password = $password;
+        $this->email = $email;
+    }
+
+}
--- src/repository/UserRepository.php
+++ src/repository/UserRepository.php
@@ -1,9 +1,65 @@
 <?php
 
-require_once 'Repository.php';
-require_once __DIR__.'/../models/User.php';
+require_once 'src/repository/Repository.php';
+require_once 'src/models/User.php';
 
 class UserRepository extends Repository
 {
+    public function getUser(string $email): ?User
+    {
+        $stmt = $this->database->connect()->prepare(
+            '
+                SELECT public.users.nickname,
+                       public.users.email,
+                       public.users.password,
+                FROM public.users
+                WHERE public.users.email = :email
+            '
+        );
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user == false) {
+            return null;
+        }
+
+        return new User(
+            $user['password'],
+            $user['email'],
+            $user['nickname'],
+        );
+    }
+
+    public function saveUser(User $user): string
+    {
+        $email = $user->getEmail();
+        if ($this->getUser($email) != null) {
+            return "User already exist";
+        }
+        try {
+            $stmt = $this->database->connect()->prepare(
+                '
+                INSERT INTO public.users(password, email, nickname)
+                VALUES (:password, :email, :nickname)
+            '
+            );
+
+            $password = $user->getPassword();
+            $email = $user->getEmail();
+            $name = $user->getNickname();
+
+            $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':nickname', $name, PDO::PARAM_STR);
+
+            $stmt->execute();
+
+            return "User created successfully";
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
 
 }
