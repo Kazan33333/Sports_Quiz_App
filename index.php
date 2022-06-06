73978208246d28af8e48c5a6dfc4e50d3795bffc
Kazan33333 <kazan.kk@gmail.com>
2022-06-06 23:28:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+require 'Routing.php';
+
+$path = $_SERVER['REQUEST_URI'];
+$path = trim($path, '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('main_menu', 'DefaultController');
+Router::get('solo_game', 'DefaultController');
+Router::get('play_with_friends', 'DefaultController');
+Router::get('top_100', 'DefaultController');
+Router::get('add_question', 'DefaultController');
+
+Router::post('login', 'SecurityController');
+Router::post('logout', 'SecurityController');
+Router::post('register', 'SecurityController');
+
+Router::run($path);
