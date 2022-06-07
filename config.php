3479534cbd10309dc3f3bbb3037bbc98f8a17ab0
kojderszymon <vaser1999@gmail.com>
2022-06-07 02:29:43 +0200

    Fix login

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'jrwwdvqbpfwgpu';
-const PASSWORD = '79ce45c5eb485f59ce82aa9e9677f0dfa72e184589efb9db67878c95283e4371';
-const HOST = 'ec2-52-214-23-110.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd8iik4fkf9uf8p';
