376295bb5391867a4f12bfd8702881f4d7874e46
kojderszymon <vaser1999@gmail.com>
2022-06-12 15:47:19 +0200

    Quiz game

--- src/controllers/QuizController.php
+++ src/controllers/QuizController.php
@@ -6,13 +6,22 @@ require_once __DIR__.'/../repository/QuizRepository.php';
 
 class QuizController extends AppController
 {
+    private QuizRepository $quizRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->quizRepository = new QuizRepository();
+    }
+
     public function addQuizQuestions(Quiz $quiz): Quiz
     {
+        $max_id_questions = $this->quizRepository->get_max_id_question();
         for($i = 0; $i < $quiz->getCountOfQuestions(); $i++)
         {
-            $quiz->add_question_to_quiz(rand(0, get_max_id_question()));
+            $quiz->add_question_to_quiz(rand(0, $max_id_questions));
         }
 
-        return quiz;
+        return $quiz;
     }
 }
